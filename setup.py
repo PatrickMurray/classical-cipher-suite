2ba226c651ee89b2556b99efbe08a89bfd09de2e
Patrick Murray <patrickm129@gmail.com>
2019-09-27 17:50:02 -0400

    progress

--- setup.py
+++ setup.py
@@ -4,7 +4,17 @@
 import setuptools
 
 
-setuptools.setup(
-    name='ccs',
-    packages=setuptools.find_packages()
-)
+with open('README.md') as handler:
+    readme_markdown = handler.read()
+    
+    setuptools.setup(
+        name='Classical Cipher Suite',
+        version='0.0.1',
+        author='Patrick Murray',
+        author_email='patrick@murray.systems',
+        description='A collection of neat historical ciphers and tooling to use them for fun and profit.',
+        long_description=readme_markdown,
+	long_description_content_type='text/markdown',
+	url='https://github.com/PatrickMurray/classical-cipher-suite',
+	packages=setuptools.find_packages()
+    )
